201a27176169e20b33298fea1a880517a9416551
jeanbaptistedeborman <jb@noloading.com>
2013-12-11 12:05:11 +0100

    Revert "Revert "Removing alerts""
    This reverts commit fa861e9a36523849432478353f592a61bd5d1e91.

--- scripts/dev/utils.js
+++ scripts/dev/utils.js
@@ -125,11 +125,13 @@ var UserAgent = {
         {
                 "use strict";
                 var rv = -1; // Return value assumes failure.
-                if (navigator.appName == 'Microsoft Internet Explorer') {
+                if (navigator.appName === 'Microsoft Internet Explorer') {
                         var ua = navigator.userAgent;
                         var re = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
-                        if (re.exec(ua) != null)
+                        if (re.exec(ua) !== null) {
                                 rv = parseFloat(RegExp.$1);
+                                
+                            }
                 }
                 return rv;
         }
