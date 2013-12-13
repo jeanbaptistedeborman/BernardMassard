db694313738cc6f0c9578072f3c04e052e33eeac
Jean-Baptiste de Borman <jb@noloading.com>
2013-12-13 12:14:01 +0100

    Enter on Like : test

--- scripts/facebook/facebook.js
+++ scripts/facebook/facebook.js
@@ -1,71 +1,73 @@
 /*global FB*/
 
 Facebook = {
-    userInfo : {},
-    authEventSubscribed_bool : false,
-    initCallbackFunction : "",
-    launchInitSequence : function(callbackFunction) {"use strict";
+	userInfo : {},
+	authEventSubscribed_bool : false,
+	initCallbackFunction : "",
+	launchInitSequence : function(callbackFunction) {"use strict";
 
-        //alert ("init");
-        this.initCallbackFunction = callbackFunction;
+		//alert ("init");
+		this.initCallbackFunction = callbackFunction;
 
-        FB.init({
-            appId : '669311946433468',
-            status : true, // check login status
-            cookie : true, // enable cookies to allow the server to access the session
-            xfbml : true // parse XFBML
-        });
-        if (!this.authEventSubscribed_bool) {
-            this.authEventSubscribed_bool = true;
+		FB.init({
+			appId : '669311946433468',
+			status : true, // check login status
+			cookie : true, // enable cookies to allow the server to access the session
+			xfbml : true // parse XFBML
+		});
+		if (!this.authEventSubscribed_bool) {
+			this.authEventSubscribed_bool = true;
 
-            FB.Event.subscribe('edge.create', function(href, widget) {
-                alert('You liked the URL: ' + href);
-            });
+		FB.Event.subscribe('edge.create', function(href, widget) {
+				Facebook.onLike (); 
+			
+				
+			});
 
-            FB.Event.subscribe('auth.authResponseChange', function(response) {
+			FB.Event.subscribe('auth.authResponseChange', function(response) {
 
-                if (response.status === 'connected') {
-                    Facebook.getUserInfo();
-                    Facebook.initCallbackFunction();
-                    //callback();
-                } else if (response.status === 'not_authorized') {
+				if (response.status === 'connected') {
+					Facebook.getUserInfo();
+					Facebook.initCallbackFunction();
+					//callback();
+				} else if (response.status === 'not_authorized') {
 
-                    FB.login();
-                } else {
+					FB.login();
+				} else {
 
-                    FB.login();
-                }
-            });
-        }
-    },
+					FB.login();
+				}
+			});
+		}
+	},
 
-    checkLike : function() {"use strict";
+	checkLike : function() {"use strict";
 
-        FB.api({
-            method : "pages.isFan",
-            page_id : "607595305979967"
-        }, function(response) {
-            if (response) {
-                Facebook.onLike();
-            } else {
-                Facebook.onDoesNotLike();
-            }
-        });
+		FB.api({
+			method : "pages.isFan",
+			page_id : "607595305979967"
+		}, function(response) {
+			if (response) {
+				Facebook.onLike();
+			} else {
+				Facebook.onDoesNotLike();
+			}
+		});
 
-    },
-    getUserInfo : function() {"use strict";
+	},
+	getUserInfo : function() {"use strict";
 
-        FB.api('/me', function(response) {
-            Facebook.userInfo = response;
-        });
+		FB.api('/me', function(response) {
+			Facebook.userInfo = response;
+		});
 
-    },
-    onLike : function() {"use strict";
-        //Event Placeholder;
+	},
+	onLike : function() {"use strict";
+		//Event Placeholder;
 
-    },
-    onDoesNotLike : function() {"use strict";
-        //Event Placeholder;
+	},
+	onDoesNotLike : function() {"use strict";
+		//Event Placeholder;
 
-    }
+	}
 };
