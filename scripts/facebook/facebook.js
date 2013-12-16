f0700312c0a6c34ede8be26fe9eb7a5755e6db01
Jean-Baptiste de Borman <jb@noloading.com>
2013-12-16 15:02:32 +0100

    test

--- scripts/facebook/facebook.js
+++ scripts/facebook/facebook.js
@@ -6,8 +6,6 @@ Facebook = {
 	authEventSubscribed_bool : false,
 	initCallbackFunction : "",
 	launchInitSequence : function(callbackFunction) {"use strict";
-
-		alert ("init , scope changed");
 		this.initCallbackFunction = callbackFunction;
 	alert ("app id" + Facebook.appId);  
 		FB.init({
@@ -16,22 +14,21 @@ Facebook = {
 			status : true, // check login status
 			cookie : true,
 			scope : "user_likes",
-
 			xfbml : true // parse XFBML
 		});
 		if (!this.authEventSubscribed_bool) {
 			this.authEventSubscribed_bool = true;
 
 			FB.Event.subscribe('edge.create', function(href, widget) {
-				Facebook.onLike();
+				
+				Facebook.checkLike ();
 
 			});
 
 			FB.Event.subscribe('auth.authResponseChange', function(response) {
-				trace ("FACEBOOK AOTH" + response);
 				if (response.status === 'connected') {
 					Facebook.getUserInfo();
-					Facebook.initCallbackFunction();
+			
 					//callback();
 				} else if (response.status === 'not_authorized') {
 
@@ -42,7 +39,7 @@ Facebook = {
 				}
 			});
 		}
-		FB.login();
+	
 	},
 
 	checkLike : function() {"use strict";
@@ -51,6 +48,7 @@ Facebook = {
 		FB.api('/me/likes/65692241192', function(response) {
 			trace("Like : " + response.data);
 			if (response.data.length == 1) {
+				alert ('page liked'); 
 				Facebook.onLike();
 			} else {
 				Facebook.onDoesNotLike();
@@ -62,9 +60,11 @@ Facebook = {
 
 	},
 	getUserInfo : function() {"use strict";
-	alert ('connected user info'); 
+	alert ('connected getting user info'); 
 
 		FB.api('/me', function(response) {
+			alert (response); 
+			Facebook.checkLike (); 
 			Facebook.userInfo = response;
 			Facebook.onUserInfo (); 
 		});
