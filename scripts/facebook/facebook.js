c2d515e47e4e28553e2756ff32a45d2faecef9b1
Jean-Baptiste de Borman <jb@noloading.com>
2013-12-17 16:15:40 +0100

    reload

--- scripts/facebook/facebook.js
+++ scripts/facebook/facebook.js
@@ -1,43 +1,59 @@
 /*global FB*/
 
 Facebook = {
-	attempts_num:0, 
+	reload_bool:false,  
+	attempts_num : 0,
 	appId : undefined,
 	userInfo : {},
 	authEventSubscribed_bool : false,
 	initCallbackFunction : "",
 	launchInitSequence : function(callbackFunction) {"use strict";
 		this.initCallbackFunction = callbackFunction;
-	//alert ("app id" + Facebook.appId);  
+		//alert ("app id" + Facebook.appId);
 		FB.init({
-		
+
 			appId : Facebook.appId,
 			status : true, // check login status
 			cookie : true,
 			scope : "user_likes",
 			xfbml : true // parse XFBML
 		});
+
+		FB.getLoginStatus(function(response) {
+			if (response.status === 'connected') {
+
+				var uid = response.authResponse.userID;
+				var accessToken = response.authResponse.accessToken;
+			} else if (response.status === 'not_authorized') {
+				reload_bool = true; 
+
+			} else {
+				reload_bool = true; 
+
+			}
+		});
+
 		if (!this.authEventSubscribed_bool) {
 			this.authEventSubscribed_bool = true;
 
 			FB.Event.subscribe('edge.create', function(href, widget) {
-				//trace ("event received"); 
-				
+				//trace ("event received");
+
 				Facebook.onLike();
 
 			});
 
 			FB.Event.subscribe('auth.authResponseChange', function(response) {
 				Facebook.attempts_num++;
-				alert (Facebook.attempts_num);
-				if (Facebook.attempts_num > 1) {
-					
-					window.location.reload (); 
-				} 
-				
+				//alert(Facebook.attempts_num);
+				if (reload_bool) {
+
+					window.location.reload();
+				}
+
 				if (response.status === 'connected') {
 					Facebook.getUserInfo();
-			
+
 					//callback();
 				} else if (response.status === 'not_authorized') {
 
@@ -48,7 +64,7 @@ Facebook = {
 				}
 			});
 		}
-	
+
 	},
 
 	checkLike : function() {"use strict";
@@ -56,11 +72,10 @@ Facebook = {
 
 		FB.api('/me/likes/65692241192', function(response) {
 			trace("Like : ");
-			
-			
+
 			if (response.data.length !== 0) {
-				trace (response.data); 
-				//alert ('page liked'); 
+				trace(response.data);
+				//alert ('page liked');
 				Facebook.onLike();
 			} else {
 				Facebook.onDoesNotLike();
@@ -68,24 +83,21 @@ Facebook = {
 			}
 		});
 
-		
-
 	},
 	getUserInfo : function() {"use strict";
-	//alert ('connected getting user info'); 
+		//alert ('connected getting user info');
 
 		FB.api('/me', function(response) {
-			//alert (response); 
-			Facebook.checkLike (); 
+			//alert (response);
+			Facebook.checkLike();
 			Facebook.userInfo = response;
-			Facebook.onUserInfo (); 
+			Facebook.onUserInfo();
 		});
 
 	},
-	onUserInfo:function  () {"use strict";
-		
-		
-},
+	onUserInfo : function() {"use strict";
+
+	},
 	onLike : function() {"use strict";
 		//Event Placeholder;
 
