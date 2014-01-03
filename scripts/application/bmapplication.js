ee73c417e98876af07357d1366c7f2b16d9cd1cc
Jean-Baptiste de Borman <jb@noloading.com>
2014-01-03 13:19:59 +0100

    Revert "End of action screen"
    This reverts commit 5923f3c0af53601df8966fdfbe1d01e9d8944916.

--- scripts/application/bmapplication.js
+++ scripts/application/bmapplication.js
@@ -2,216 +2,223 @@
 /*global SpriteAnimation, BMGame, UserAgent, Facebook,  PopupManager */
 
 $(document).ready(function() {"use strict";
-    //alert("with login - corrected 2");
-var endOfAction_bool = true; 
-    $('#connect').bind('click', function() {
+	//alert("with login - corrected 2");
+	
+	$('#connect').bind('click', function() {
 
-    });
-    $('.fb-like').css("top", -5000);
-    $('.fb-share-button').css("top", -5000);
+	});
+	$('.fb-like').css ("top", -5000);
+	$('.fb-share-button').css ("top", -5000);
+	
 
-    function preloadImage(url) {
-        try {
-            var _img = new Image();
-            _img.src = url;
-
-        } catch (e) {
-        }
-    }
-
-    if (UserAgent.iOS()) {
-
-    }
-    //$("body").hide ();
-
-    Facebook.onUserInfo = function() {
-        //alert ('onuserinfo')
-        $('.fb-login-button').hide();
-        $('.fb-like').css("top", "");
-
-    };
-
-    preloadImage("graphic/game/animation/icebreak.png");
-    /*if (UserAgent.anyMobile() && false) {
-
-     } else {
-
-     }
-     */
-
-    $('.js_continue').hide();
-    var ui_$ = $("#ui");
-    var popups_$ = ui_$.find('#popups');
-
-    if (String(window.location.href).indexOf("azurewebsites") !== -1) {
-
-        Facebook.appId = '669311946433468';
-        $('.js_continue').show();
-    } else {
-        Facebook.appId = '1384449105138296';
-
-    }( function(d) {
-
-            var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
-            if (d.getElementById(id)) {
-                return;
-            }
-            js = d.createElement('script');
-            js.id = id;
-            js.async = true;
-            js.src = "https://connect.facebook.net/fr_FR/all.js";
-            ref.parentNode.insertBefore(js, ref);
-        }(document));
-
-    window.fbAsyncInit = function() {
-
-        //Facebook.launchInitSequence(Facebook.checkLike());
-
-    };
-
-    $("#gameContainer").hide();
-    $(".js_continue").bind("click", function() {
-
-        PopupManager.close($('#intro'));
-
-    });
-
-    popups_$.detach();
-    PopupManager.container_$ = ui_$;
-    PopupManager.popups_$ = popups_$;
-    if (!endOfAction_bool) {
-    PopupManager.display("intro");
-    
-  } else {
-       PopupManager.display("endOfAction");
-      
-      
-  }
-    Facebook.onDoesNotLike = function() {
-        //trace ("application on does not like");
-
-    };
-
-    Facebook.onLike = function() {
-        if (false) {
-            $('.fb-like').hide();
-            $('.js_continue').show();
-            $('.js_continue').css("display", "inline-block");
-        }
-    };
-
-    function manageForm() {
-
-        //alert("manageForm");
-
-        $("#form").find(".button").bind("click tap touch", function() {
-
-            var form_$ = $("form"), data = {
-
-            }, send_bool = true, n, mail_bool = true;
-
-            form_$.find('input').each(function(index, element) {
-                //alert ("each");
-
-                var element_$ = $(element);
-                var value = element_$.val();
-                if (value === "on") {
-                    value = element_$.prop('checked');
-
-                }
-
-                data[element_$.attr("name")] = value;
-
-            });
-            data.answer = form_$.find("input[type='radio']:checked").val();
-
-            //trace(data);
-
-            for (n in data) {
-                var value = data[n];
-                if (value === undefined || value === null || value === "" || value == false) {
-                    send_bool = false;
-                }
-
-            }
-
-            function validateForm() {
-                var result = true;
-                var email_str = form_$.find("input[type='email']").val();
-                var atpos = email_str.indexOf("@");
-                var dotpos = email_str.lastIndexOf(".");
-                if (atpos < 1 || dotpos < atpos + 2 || dotpos + 2 >= email_str.length) {
-                    //alert("Not a valid e-mail address");
-                    result = false;
-                }
-                return (result)
-            }
-
-            mail_bool = validateForm();
-
-            //trace("Facebook.userInfo.id : " + Facebook.userInfo.id);
-            data.facebook_id = Facebook.userInfo.id;
-
-            if (send_bool && mail_bool) {
-
-                $.ajax({
-                    url : "api/subscription.php",
-                    type : "POST",
-                    data : data
-                }).done(function() {
-                    PopupManager.close($("#form"));
-
-                });
-
-            } else {
-                if (!mail_bool) {
-                    alert("L'adresse e-mail n'est pas valide.");
-
-                } else {
-                    alert("Tous les champs doivent être remplis.");
-
-                }
-
-            }
-
-        });
-
-    }
+	function preloadImage(url) {
+		try {
+			var _img = new Image();
+			_img.src = url;
+			
+		} catch (e) {
+		}
+	}
+	if (UserAgent.iOS ()) {
+		
+		
+		
+		
+	}
+	//$("body").hide (); 
 
 
-    PopupManager.onClosePopup = function() {
-        switch (PopupManager.lastIdName) {
-            case "endOfAction":
 
-                break;
-            case "intro":
+	Facebook.onUserInfo = function() {
+		//alert ('onuserinfo')
+		$('.fb-login-button').hide();
+		$('.fb-like').css ("top", ""); 
 
-                PopupManager.display('howToPlay');
+	};
 
-                break;
+	preloadImage("graphic/game/animation/icebreak.png");
+	/*if (UserAgent.anyMobile() && false) {
 
-            case "howToPlay" :
 
-                BMGame.init();
-                $('body').addClass('bg2');
+	} else {
 
-                break;
+	}
+	*/
+	
+	 
 
-            case "negativeResult":
-            case "positiveResult":
-                PopupManager.display('form');
-                manageForm();
-                break;
+	$('.js_continue').hide (); 
+	var ui_$ = $("#ui");
+	var popups_$ = ui_$.find('#popups');
+	
 
-            case "form":
-                PopupManager.display('thankYou');
+	if (String(window.location.href).indexOf("azurewebsites") !== -1) {
 
-                $('#thankYou').append($('.fb-share-button').show().css("top", ""));
+		Facebook.appId = '669311946433468';
+		$('.js_continue').show (); 
+	} else {
+		Facebook.appId = '1384449105138296';
 
-                break;
+	}
+	
+	
+	( function(d) {
 
-        }
+			var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+			if (d.getElementById(id)) {
+				return;
+			}
+			js = d.createElement('script');
+			js.id = id;
+			js.async = true;
+			js.src = "https://connect.facebook.net/fr_FR/all.js";
+			ref.parentNode.insertBefore(js, ref);
+		}(document));
 
-    };
+
+	window.fbAsyncInit = function() {
+
+		Facebook.launchInitSequence(Facebook.checkLike());
+
+	};
+
+	$("#gameContainer").hide();
+	$(".js_continue").bind("click", function() {
+
+		PopupManager.close($('#intro'));
+
+	});
+
+	popups_$.detach();
+	PopupManager.container_$ = ui_$;
+	PopupManager.popups_$ = popups_$;
+	PopupManager.display("intro");
+	Facebook.onDoesNotLike = function() {
+		//trace ("application on does not like"); 
+
+	};
+
+	Facebook.onLike = function() {
+
+		$('.fb-like').hide();
+		$('.js_continue').show ();
+		$('.js_continue').css ("display", "inline-block");  
+
+	};
+
+	function manageForm() {
+
+		//alert("manageForm");
+
+		$("#form").find(".button").bind("click tap touch", function() {
+
+			var form_$ = $("form"), data = {
+
+			}, send_bool = true, n, mail_bool=true;
+
+			form_$.find('input').each(function(index, element) {
+				//alert ("each");
+
+				var element_$ = $(element);
+				var value = element_$.val();
+				if (value === "on") {
+					value = element_$.prop('checked');
+
+				}
+
+				data[element_$.attr("name")] = value;
+
+			});
+			data.answer = form_$.find("input[type='radio']:checked").val();
+
+			//trace(data);
+
+			for (n in data) {
+				var value = data[n];
+				if (value === undefined || value === null || value === "" || value == false) {
+					send_bool = false;
+				}
+
+			}
+			
+
+			function validateForm() {
+				var result = true; 
+				var email_str = form_$.find("input[type='email']").val();
+				var atpos = email_str.indexOf("@");
+				var dotpos = email_str.lastIndexOf(".");
+				if (atpos < 1 || dotpos < atpos + 2 || dotpos + 2 >= email_str.length) {
+					//alert("Not a valid e-mail address");
+					result = false;
+				}
+				return (result)
+			}
+			mail_bool = validateForm(); 
+
+			//trace("Facebook.userInfo.id : " + Facebook.userInfo.id);
+			data.facebook_id = Facebook.userInfo.id;
+			
+		
+
+			if (send_bool && mail_bool) {
+
+				$.ajax({
+					url : "api/subscription.php",
+					type : "POST",
+					data : data
+				}).done(function() {
+					PopupManager.close($("#form"));
+
+				});
+
+			} else {
+				if (!mail_bool) {
+					alert("L'adresse e-mail n'est pas valide.");
+					
+				} else {
+					alert("Tous les champs doivent être remplis.");
+					
+				}
+				
+				
+
+			}
+
+		});
+
+	}
+
+
+	PopupManager.onClosePopup = function() {
+		switch (PopupManager.lastIdName) {
+			case "intro":
+
+				PopupManager.display('howToPlay');
+
+				break;
+
+			case "howToPlay" :
+
+				BMGame.init();
+				$('body').addClass('bg2');
+
+				break;
+
+			case "negativeResult":
+			case "positiveResult":
+				PopupManager.display('form');
+				manageForm();
+				break;
+
+			case "form":
+				PopupManager.display('thankYou');
+			
+				$('#thankYou').append (	$('.fb-share-button').show ().css ("top", ""));
+	
+				break;
+
+		}
+
+	};
 
 });
